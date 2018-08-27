de78b79add6f3fd7879f155f5eb9a5bd2929f70c
yy2792 <yy2792@columbia.edu>
2018-08-27 01:42:52 -0400

    fixed test file

--- test.py
+++ test.py
@@ -3,10 +3,9 @@ from Deal_Date import deal_date as dd
 print("-CDS-1-20/06/22- 58 50.000.000.00 EUR 14--04/2018 20--06/2022")
 print(dd.get_two_dates("-CDS-1-20/06/22- 58 50.000.000.00 EUR 14--04/2018 20--06/2022","%d--%m/%Y"))
 
-print("-CDS-5-20/12/18-JC PENNEY SENIOR U 19 2.000.000.00 USD 14/04/2018 20/12/2018")
-print(dd.get_two_dates("-CDS-1-20/06/22- 58 50.000.000.00 EUR 14--04/2018 20--06/2022","%d--%m/%Y"))
 
-print(dd.find_date('11**JUN20', "%d**%b%Y"))
+print("-CDS-1-20/06/22- 58 50.000.000.00 EUR 14--04/2018 20--06/2022","%d--%m/%Y")
+print(dd.get_two_dates("-CDS-1-20/06/22- 58 50.000.000.00 EUR 14--04/2018 20--06/2022","%d--%m/%Y"))
 
 print("-CDS-5-20/12/18-JC PENNEY SENIOR U 19 2.000.000.00 USD 14**042018 20**122018")
 print(dd.get_two_dates("-CDS-1-20/06/22- 58 50.000.000.00 EUR 14**042018 20**122018","%d**%m%Y"))
